54f8d215f0a4fa93ab0a623c21491938cdf5392b
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2020-07-08 14:07:16 +0200

    Cleanup

--- packages/core/core/src/BundleGraph.js
+++ packages/core/core/src/BundleGraph.js
@@ -154,11 +154,7 @@ export default class BundleGraph {
         return;
       }
 
-      if (
-        node.type === 'dependency' &&
-        node.value.symbols != null &&
-        node.excluded
-      ) {
+      if (node.type === 'dependency' && this.isDependencySkipped(node.value)) {
         actions.skipChildren();
         return;
       }
@@ -992,13 +988,11 @@ export default class BundleGraph {
       return {
         asset,
         exportSymbol: symbol,
-        symbol:
-          identifier ??
-          (skipped
-            ? false
-            : found || asset.symbols.has('*')
-            ? null
-            : undefined),
+        symbol: skipped
+          ? false
+          : found || asset.symbols.has('*')
+          ? null
+          : identifier,
         loc: asset.symbols.get(symbol)?.loc,
       };
     }
